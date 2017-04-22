4d53589c8c59e1df0711b2af79920765a3b9534c
ebifrier <ebifrier@gmail.com>
2017-04-23 02:00:52 +0900

    Modified run-loop.sh

--- src/run-loop.sh
+++ src/run-loop.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
-threads=`nproc`
+threadSize=`nproc`
 random=`od -vAn -N4 -tu4 < /dev/random`
 loginName="lkujira_$(($random % 1000))"
 host="godwhale.ddns.net"
 port=4082
 
+echo -n "please input thread size (default is $threadSize): "
+read threadSize2
+
+if [ ! x"$threadSize2" = x"" ]; then
+    threadSize="$threadSize2"
+fi
+
 while :
 do
-    echo "./godwhale_child $host $port $loginName $threads"
+    ./godwhale_child "$host" "$port" "$loginName" "$threadSize"
+    sleep 10
 done
