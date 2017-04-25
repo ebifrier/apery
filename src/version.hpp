cfcfb12e72406dadd7549041f7ecf3aaf16e6124
ebifrier <ebifrier@gmail.com>
2017-04-26 00:17:28 +0900

    エンジンをバージョンアップ

--- src/version.hpp
+++ src/version.hpp
@@ -2,7 +2,7 @@
 #ifndef APERY_VERSION_HPP
 #define APERY_VERSION_HPP
 
-const std::string SlaveName = "GodwhaleChild-4.0.1";
+const std::string SlaveName = "GodwhaleChild-4.0.2";
 constexpr int LoginNameMaxLength = 12;
 
 #endif
