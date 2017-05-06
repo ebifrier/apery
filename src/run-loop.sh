c78bcb47906e4f0853b3bb7f00e0e154d3a03c76
ebifrier <ebifrier@gmail.com>
2017-05-06 16:03:56 +0900

    Compiled on Mac

--- src/run-loop.sh
+++ src/run-loop.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 
-threadSize=`nproc`
 random=`od -vAn -N4 -tu4 < /dev/random`
 loginName="lkujira_$(($random % 1000))"
 host="godwhale.ddns.net"
 port=4082
+
+if [ "$(uname)" = "Darwin" ]; then
+    # under Mac OS X platform
+    threadSize=`sysctl -n hw.ncpu`
+elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
+    # under GNU/Linux platform
+    threadSize=`nproc`
+elif [ "$(expr substr $(uname -s) 1 10)" = "MINGW32_NT" ]; then
+    # under 32 bits Windows NT platform
+    threadSize=$NUMBER_OF_PROCESSORS
+elif [ "$(expr substr $(uname -s) 1 10)" = "MINGW64_NT" ]; then
+    # under 64 bits Windows NT platform
+    threadSize=$NUMBER_OF_PROCESSORS
+else
+    threadSize=1
+fi
 
 echo -n "please input thread size (default is $threadSize): "
 read threadSize2
